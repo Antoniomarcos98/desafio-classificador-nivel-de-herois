65fc1e3f75f7b01ae775944bacd460ff5a37e574
Antoniomarcos98 <marcosmatosvieira@gmail.com>
2024-08-22 16:11:50 -0300

    meu primeiro commit

---
+++ index.js
@@ -0,0 +1,15 @@
+let nomesHerois = ["Homem de Ferro" , "Mulher Maravilha" , "Homem Aranha" , "Capitã Marvel" ,
+     "Hulk" , "Capitão America" , "Thor" , "Flash"]
+
+let sexoNivelHerois = [
+    
+["Homem de Ferro", "M", 1000],
+["Mulher Maravilha", "F", 2000],
+["Homem Aranha", "M", 4000],
+["Capitã Marvel", "F", 6000],
+["Hulk", "M" 8000],
+["Capitão America", "M", 9000],
+["Thor", "M", 10.000],
+["Flash", "M", 10.001]
+
+]
